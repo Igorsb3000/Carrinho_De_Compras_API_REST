9e5688944cf69f49cf89142c6822d61461b1b732
Eduardo Vaz <dawerton.vaz@jfal.jus.br>
2023-11-30 10:40:38 -0300

    ajustando testes parametrizados

--- src/test/java/br/com/atlantic/api/domain/service/CarrinhoServiceTests.java
+++ src/test/java/br/com/atlantic/api/domain/service/CarrinhoServiceTests.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-@ActiveProfiles("dev")
+@ActiveProfiles("test")
 @SpringBootTest
 public class CarrinhoServiceTests {
 
